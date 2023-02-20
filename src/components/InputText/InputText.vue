dffaa2669b5d0bd047a967e863d67808f34231f4
“kudou” <h.kudou@ebacorp.jp>
2023-02-20 09:02:29 +0900

    test

---
+++ src/components/InputText/InputText.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-text-field label="First name" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Emit, Vue } from "vue-property-decorator";
+
+@Component
+export default class InputText extends Vue {
+  // @Prop({ default: false })
+  // public disabled!: boolean;
+
+  // @Prop({ default: "" })
+  // public classMethod!: string;
+
+  // @Prop({ default: "" })
+  // public text!: string;
+
+  @Emit("bottonClick")
+  public onClick() {
+    //
+  }
+}
+</script>
+
+<style lang="scss">
+.v-btn {
+  &.v-btn--has-bg {
+    &.base {
+      color: #fff;
+      background-color: #0069d9;
+    }
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,16 +7,19 @@
       :classMethod="'base'"
       :text="'Button'"
     />
+    <InputText />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Button from "@/components/Button/Button.vue";
+import InputText from "@/components/InputText/InputText.vue";
 
 @Component({
   components: {
     Button,
+    InputText,
   },
 })
 export default class Home extends Vue {
