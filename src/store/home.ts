90efde337cfa7aab0e1479c6bc809565d2d8cfbf
“kudou” <h.kudou@ebacorp.jp>
2023-02-20 14:49:36 +0900

    test

--- src/store/home.ts
+++ src/store/home.ts
@@ -3,27 +3,27 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-type State = {
+type InitialState = {
   inputText1: string;
   inputText2: string;
 };
 
-const initData: State = {
+const initialState: InitialState = {
   inputText1: "test1",
   inputText2: "test2",
 };
 
 export default {
-  state: initData,
+  state: initialState,
   getters: {
-    getState: (state: State) => {
+    getState: (state: InitialState) => {
       return state;
     },
   },
   mutations: {
-    setCount(state: any, newCount: any) {
-      state.count = newCount;
-    },
+    // setCount(state: any, newCount: any) {
+    //   state.count = newCount;
+    // },
   },
   actions: {
     // setCountAction({ commit: any }, newCount: any) {
