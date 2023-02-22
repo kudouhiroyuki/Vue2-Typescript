dcf6940ac7ffa863c8f6205bd31b244a2b1bbda3
“kudou” <h.kudou@ebacorp.jp>
2023-02-22 10:13:35 +0900

    test

--- src/pages/top/Hair.vue
+++ src/pages/top/Hair.vue
@@ -10,6 +10,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import TheHeader from "@/components/TheHeader.vue";
 import SearchBox from "./components/SearchBox.vue";
+import { MenusApi } from "@/api/menus";
 
 @Component({
   components: {
@@ -19,16 +20,26 @@ import SearchBox from "./components/SearchBox.vue";
 })
 export default class hairTop extends Vue {
   created() {
+    this.getMenusApi();
     this.$store.dispatch("commonState", { currentNav: "hair" });
   }
   mounted() {
-    // console.log("mounted");
+    console.log(this.$store);
   }
   updated() {
     // console.log("updated");
   }
   destroyed() {
     // console.log("destroyed");
+  }
+
+  public getMenusApi() {
+    const menusApi = new MenusApi();
+    menusApi.get({ category_id: 1 }).then((res) => {
+      this.$store.dispatch("topPageState", {
+        menus: res.menus,
+      });
+    });
   }
 }
 </script>
--- src/store/topPage.ts
+++ src/store/topPage.ts
@@ -1,19 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { Commit } from "vuex";
-import { MenusApi } from "@/api/menus";
 
 Vue.use(Vuex);
 
 type TopPageState = {
   area: string;
   keyword: string;
-  [key: string]: string;
+  menus: { [key: string]: string }[];
+  [key: string]: string | { [key: string]: string }[];
 };
 
 const state: TopPageState = {
   area: "",
   keyword: "",
+  menus: [],
 };
 
 export default {
@@ -33,10 +34,6 @@ export default {
   actions: {
     topPageState({ commit }: { commit: Commit }, newState: TopPageState) {
       commit("topPageState", newState);
-      const menusApi = new MenusApi();
-      menusApi.get({ category_id: 1 }).then((res) => {
-        console.log(res);
-      });
     },
   },
 };
