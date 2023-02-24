f5e113462d79123adf8c7780aededc080743a7db
“kudou” <h.kudou@ebacorp.jp>
2023-02-24 11:48:29 +0900

    test

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,6 +1,9 @@
 export interface ToSnakeCase {
   [key: string]: string | number;
 }
+export interface ToCamelCase {
+  [key: string]: string | number;
+}
 
 export class BaseApi {
   public toSnakeCase(params: ToSnakeCase): object {
@@ -12,6 +15,23 @@ export class BaseApi {
         .toLowerCase();
       result[convertKey] = params[baseKey];
     });
+    return result;
+  }
+  public toCamelCase(resData: any): any {
+    const result: any = [];
+    resData.forEach((value: any, index: any) => {
+      Object.keys(value).forEach((k: any, v: any) => {
+        const convertKey = k
+          .split("_")
+          .map(function (word: any, index: any) {
+            if (index === 0) return word.toLowerCase();
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+          })
+          .join("");
+      });
+      result.push({ aaaa: 111 });
+    });
+    console.log(result);
     return result;
   }
 }
--- src/api/menus.ts
+++ src/api/menus.ts
@@ -1,12 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 import { BaseApi } from "@/api/base";
 
-export interface MenuEntity {
-  menuId: number;
-  menuName: string;
-  menuImage_url: string;
-}
-
 export interface MenusGetRequestBaseDto {
   categoryId: number;
   [key: string]: string | number;
@@ -16,9 +10,38 @@ export interface MenusGetRequestResultDto {
   category_id: number;
 }
 
-export interface MenusGetResponseDto {
-  menus: MenuEntity[];
+export interface MenusGetResponseBaseDto {
+  menus: {
+    menu_id: number;
+    menu_name: string;
+    menu_image_url: string;
+  }[];
+  [key: string]: object;
 }
+
+export interface MenusGetResponseResultDto {
+  menus: {
+    menu_id: number;
+    menu_name: string;
+    menu_image_url: string;
+  }[];
+}
+
+// export interface MenusGetResponseResultDto {
+//   menuId: number;
+//   menuName: string;
+//   menuImageUrl: string;
+//   [key: string]: string | number;
+// }[]
+
+// const test: MenusGetResponseResultDto[] = [
+//   {
+//     menuId: 1,
+//     menuImageUrl: "https://beauty.tsuku2.jp/images/menus/menu_hair_01.jpg",
+//     menuName: "カット",
+//   },
+// ];
+// console.log(test);
 
 export class MenusApi extends BaseApi {
   public async getMenus(params: MenusGetRequestBaseDto) {
@@ -29,8 +52,11 @@ export class MenusApi extends BaseApi {
       params: resultParams,
     };
     return await axios(options)
-      .then((res: AxiosResponse<MenusGetResponseDto>) => {
-        return res.data.menus;
+      .then((res: AxiosResponse<MenusGetResponseBaseDto>) => {
+        // const resultRes: MenusGetResponseResultDto[] = res.data.menus;
+        const resultRes = this.toCamelCase(res.data.menus);
+        // console.log(resultRes);
+        return resultRes;
       })
       .catch((error: AxiosError<{ error: string }>) => {
         throw new Error(error.message);
