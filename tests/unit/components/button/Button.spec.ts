71fdd681a22de451f7ac26b5ff76b04bc0a190c8
“kudou” <h.kudou@ebacorp.jp>
2023-02-27 08:52:11 +0900

    test

--- tests/unit/components/button/Button.spec.ts
+++ tests/unit/components/button/Button.spec.ts
@@ -1,12 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Button from "@/components/button/Button.vue";
 
-describe("Button.vue テスト", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("components/button/Button.vue", () => {
+  test("onClick", () => {
+    const wrapper = shallowMount(Button);
+    wrapper.trigger("click");
   });
 });
