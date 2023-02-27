7ce317a339946b8a13ed222593c47039a6922c14
“kudou” <h.kudou@ebacorp.jp>
2023-02-27 10:20:15 +0900

    test

--- tests/unit/components/button/Button.spec.ts
+++ tests/unit/components/button/Button.spec.ts
@@ -2,6 +2,14 @@ import { mount } from "@vue/test-utils";
 import Button from "@/components/button/Button.vue";
 
 describe("components/button/Button.vue", () => {
+  test("props", async () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        text: "Button"
+      }
+    });
+    expect(wrapper.text()).toMatch("Button");
+  });
   test("onClick", async () => {
     const wrapper = mount(Button);
     wrapper.trigger("click");
