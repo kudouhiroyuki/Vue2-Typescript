101c441df5563e7ea56fd3f668a93e99fc6b4f6a
“kudou” <h.kudou@ebacorp.jp>
2023-02-27 15:16:33 +0900

    test

--- tests/unit/components/inputText/InputText.spec.ts
+++ tests/unit/components/inputText/InputText.spec.ts
@@ -2,6 +2,18 @@ import { mount } from "@vue/test-utils";
 import InputText from "@/components/inputText/InputText.vue";
 
 describe("components/inputText/InputText.vue", () => {
+  test("props", async () => {
+    const wrapper = mount(InputText, {
+      propsData: {
+        disabled: true,
+        classMethod: "base",
+        label: "InputText"
+      }
+    });
+    // console.log(wrapper.html());
+    expect(wrapper.find("input").attributes().disabled).toMatch("disabled");
+    expect(wrapper.find("label").text()).toMatch("InputText");
+  });
   test("onChange", async () => {
     const wrapper = mount(InputText);
     wrapper.find("input").trigger("input");
